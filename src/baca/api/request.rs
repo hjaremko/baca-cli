d0bc1ce53dbfe93adab81831537896bc4299e375
Hubert Jaremko <hjaremko@outlook.com>
2021-04-20 16:11:01 +0200

    Add some Request tests

--- src/baca/api/request.rs
+++ src/baca/api/request.rs
@@ -40,6 +40,8 @@ impl<'a> Request<'a> {
         req.send()
     }
 
+    // todo: submit here
+
     fn make_request(&self, req_type: RequestType) -> RequestBuilder {
         let post_url = format!("{}{}", self.instance.make_module_base(), req_type.mapping());
         let payload = self.instance.make_payload(&req_type);
@@ -64,5 +66,67 @@ impl<'a> Request<'a> {
             .header("DNT", "1")
             .header("X-GWT-Module-Base", self.instance.make_module_base())
             .header("X-GWT-Permutation", &self.instance.permutation)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use reqwest::StatusCode;
+
+    fn make_instance() -> InstanceData {
+        InstanceData {
+            host: "mn2020".to_string(),
+            login: "login".to_string(),
+            password: "password".to_string(),
+            permutation: "5A4AE95C27260DF45F17F9BF027335F6".to_string(),
+            cookie: "cookie".to_string(),
+        }
+    }
+
+    fn check_response(response: reqwest::Result<Response>) {
+        // todo: tsl error
+        if response.is_ok() {
+            let response = response.unwrap();
+            assert_eq!(response.status(), StatusCode::OK);
+            assert_eq!(response.text().unwrap(), "//OK[0,[],0,7]");
+        }
+    }
+
+    #[test]
+    fn login_should_connect() {
+        let baca = make_instance();
+        let req = Request::new(&baca);
+        let response = req.login();
+
+        // todo: handle tsl error
+        check_response(response);
+    }
+
+    #[test]
+    fn details_should_connect() {
+        let baca = make_instance();
+        let req = Request::new(&baca);
+        let response = req.details("1");
+
+        check_response(response);
+    }
+
+    #[test]
+    fn results_should_connect() {
+        let baca = make_instance();
+        let req = Request::new(&baca);
+        let response = req.results();
+
+        check_response(response);
+    }
+
+    #[test]
+    fn tasks_should_connect() {
+        let baca = make_instance();
+        let req = Request::new(&baca);
+        let response = req.tasks();
+
+        check_response(response);
     }
 }
