eb2e7fa68118771098428c3e2bb38543d6f49e99
Hubert Jaremko <hjaremko@outlook.com>
2021-05-10 14:13:03 +0200

    Add refresh tests, extract some common functions

--- tests/not_initialized.rs
+++ tests/not_initialized.rs
@@ -1,58 +1,20 @@
-#[macro_use]
-extern crate serial_test;
+mod util;
 
 mod not_init {
-    use assert_cmd::Command;
-    use predicates::prelude::*;
-    use std::error::Error;
-
-    mod util {
-        use assert_cmd::Command;
-        use predicates::prelude::predicate;
-        use std::error::Error;
-
-        pub fn check_command(command: &str, pattern: &str) -> Result<(), Box<dyn Error>> {
-            let mut cmd = Command::cargo_bin("baca")?;
-
-            cmd.arg(command);
-            cmd.assert()
-                // .failure() // todo: exit codes
-                .stdout(predicate::str::contains(pattern));
-            Ok(())
-        }
-    }
-
-    fn check_command(command: &str) -> Result<(), Box<dyn Error>> {
-        let pattern = "not initialized";
-        util::check_command(command, pattern)
-    }
+    use crate::util;
 
     #[test]
-    #[serial]
     fn tasks_not_initialized() -> Result<(), Box<dyn std::error::Error>> {
-        check_command("tasks")
+        util::assert_fails_if_not_initialized(&["tasks"])
     }
 
     #[test]
-    #[serial]
-    fn refresh_not_initialized() -> Result<(), Box<dyn std::error::Error>> {
-        check_command("refresh")
-    }
-
-    #[test]
-    #[serial]
     fn log_not_initialized() -> Result<(), Box<dyn std::error::Error>> {
-        check_command("log")
+        util::assert_fails_if_not_initialized(&["log"])
     }
 
     #[test]
-    #[serial]
     fn details_not_initialized() -> Result<(), Box<dyn std::error::Error>> {
-        let mut cmd = Command::cargo_bin("baca")?;
-
-        cmd.arg("details").arg("123");
-        cmd.assert()
-            .stdout(predicate::str::contains("not initialized"));
-        Ok(())
+        util::assert_fails_if_not_initialized(&["details", "123"])
     }
 }
---
+++ tests/util/mod.rs
@@ -0,0 +1,78 @@
+use assert_cmd::Command;
+use assert_fs::fixture::ChildPath;
+use assert_fs::prelude::{FileTouch, FileWriteStr, PathChild};
+use assert_fs::TempDir;
+use predicates::prelude::predicate;
+use std::env;
+use std::error::Error;
+use std::path::Path;
+
+pub trait BacaDirectoryPaths {
+    fn baca_config_file_path(&self) -> Box<Path>;
+    fn baca_task_config_file_path(&self) -> Box<Path>;
+}
+
+impl BacaDirectoryPaths for TempDir {
+    fn baca_config_file_path(&self) -> Box<Path> {
+        self.path().join(".baca/instance").into_boxed_path()
+    }
+
+    fn baca_task_config_file_path(&self) -> Box<Path> {
+        self.path().join(".baca/task").into_boxed_path()
+    }
+}
+
+pub fn set_up_command(dir: &TempDir) -> Result<Command, Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("baca")?;
+    cmd.current_dir(&dir);
+    Ok(cmd)
+}
+
+pub fn set_up_with_dir() -> Result<(TempDir, Command), Box<dyn Error>> {
+    let dir = assert_fs::TempDir::new()?;
+    let cmd = set_up_command(&dir)?;
+    Ok((dir, cmd))
+}
+
+pub fn assert_contains_pattern(command: &[&str], pattern: &str) -> Result<(), Box<dyn Error>> {
+    let (dir, mut cmd) = set_up_with_dir()?;
+
+    cmd.args(command);
+    cmd.assert()
+        // .failure() // todo: exit codes
+        .stdout(predicate::str::contains(pattern));
+
+    dir.close()?;
+    Ok(())
+}
+
+pub fn assert_fails_if_not_initialized(command: &[&str]) -> Result<(), Box<dyn Error>> {
+    let pattern = "not initialized";
+    assert_contains_pattern(command, pattern)
+}
+
+pub fn initialize_correct_workspace() -> Result<TempDir, Box<dyn std::error::Error>> {
+    let pass = env::var("BACA_PASSWORD").expect("No BACA_PASSWORD provided");
+    let (dir, mut cmd) = set_up_with_dir()?;
+
+    cmd.arg("init")
+        .args(&["-h", "mn2020", "-p", &pass, "-l", "jaremko"]);
+    cmd.assert();
+    Ok(dir)
+}
+
+pub fn make_input_file_cpp(dir: &TempDir) -> Result<ChildPath, Box<dyn std::error::Error>> {
+    let input_file = dir.child("source.cpp");
+    input_file.touch()?;
+    input_file.write_str(
+        r#"
+        \\ Hubert Jaremko
+        #include <iostream>
+        int main() {
+            std::cout << "Hello world" << std::endl;
+            return 0;
+        }
+        "#,
+    )?;
+    Ok(input_file)
+}
