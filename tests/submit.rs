e68ab94b3ca56aa86cc5bccaed5886712ec2aa62
Hubert Jaremko <hjaremko@outlook.com>
2021-05-10 14:13:03 +0200

    Add refresh tests, extract some common functions

--- tests/submit.rs
+++ tests/submit.rs
@@ -1,39 +1,10 @@
+mod util;
+
 mod submit {
-    use assert_cmd::Command;
-    use assert_fs::fixture::ChildPath;
-    use assert_fs::prelude::*;
+    use crate::util::*;
     use assert_fs::TempDir;
     use predicates::prelude::*;
-    use std::{env, fs};
-
-    // todo: move to test::util
-    fn initialize() -> Result<TempDir, Box<dyn std::error::Error>> {
-        let pass = env::var("BACA_PASSWORD")?;
-        let temp = assert_fs::TempDir::new()?;
-
-        let mut cmd = Command::cargo_bin("baca")?;
-        cmd.current_dir(&temp);
-        cmd.arg("init")
-            .args(&["-h", "mn2020", "-p", &pass, "-l", "jaremko"]);
-        cmd.assert();
-        Ok(temp)
-    }
-
-    fn make_input_file_cpp(temp: &TempDir) -> Result<ChildPath, Box<dyn std::error::Error>> {
-        let input_file = temp.child("source.cpp");
-        input_file.touch()?;
-        input_file.write_str(
-            r#"
-        \\ Hubert Jaremko
-        #include <iostream>
-        int main() {
-            std::cout << "Hello world" << std::endl;
-            return 0;
-        }
-        "#,
-        )?;
-        Ok(input_file)
-    }
+    use std::fs;
 
     fn assert_config_file_exists(temp: &TempDir) {
         let config_path = &*temp.path().join(".baca/instance");
@@ -55,26 +26,15 @@ mod submit {
 
     #[test]
     fn not_initialized() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = assert_fs::TempDir::new()?;
-        let mut cmd = Command::cargo_bin("baca")?;
-
-        cmd.current_dir(&temp);
-        cmd.arg("submit")
-            .args(&["-t", "1", "-l", "C++", "-f", "source.cpp"]);
-        cmd.assert()
-            .stdout(predicate::str::contains("not initialized"));
-
-        temp.close()?;
-        Ok(())
+        assert_fails_if_not_initialized(&["submit", "-t", "1", "-l", "C++", "-f", "source.cpp"])
     }
 
     #[test]
     fn inactive_task_should_report_error() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = initialize()?;
-        let input_file = make_input_file_cpp(&temp)?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
+        let input_file = make_input_file_cpp(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
-        cmd.current_dir(&temp);
         cmd.args(&[
             "submit",
             "-t",
@@ -91,110 +51,100 @@ mod submit {
             .stdout(predicate::str::contains("C++"))
             .stdout(predicate::str::contains("Error sending submit"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn no_task_id_should_report_error() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
-        cmd.current_dir(&temp);
         cmd.args(&["submit", "-l", "C++", "-f", "dummy.txt"]);
 
         cmd.assert()
             .stdout(predicate::str::contains("provide task_id"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn no_file_should_report_error() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
-        cmd.current_dir(&temp);
         cmd.args(&["submit", "-l", "C++", "-t", "2"]);
 
         cmd.assert()
             .stdout(predicate::str::contains("provide file"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn no_language_should_report_error() -> Result<(), Box<dyn std::error::Error>> {
-        initialize()?;
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
         cmd.args(&["submit", "-f", "dummy.txt", "-t", "2"]);
-        cmd.current_dir(&temp);
 
         cmd.assert()
             .stdout(predicate::str::contains("provide language"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn invalid_filename_should_report_error() -> Result<(), Box<dyn std::error::Error>> {
-        initialize()?;
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
         cmd.args(&["submit", "-f", "dummy.txt", "-t", "2", "-l", "C++"]);
-        cmd.current_dir(&temp);
 
         cmd.assert()
             .stdout(predicate::str::contains("Error reading source file"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn invalid_language_should_report_error() -> Result<(), Box<dyn std::error::Error>> {
-        initialize()?;
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
         cmd.args(&["submit", "-f", "dummy.txt", "-t", "2", "-l", "CPlusPlus"]);
-        cmd.current_dir(&temp);
 
         cmd.assert()
             .stdout(predicate::str::contains("cplusplus is not yet supported"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn invalid_task_should_report_error() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
         cmd.args(&["submit", "-f", "dummy.txt", "-t", "2123123", "-l", "C++"]);
-        cmd.current_dir(&temp);
 
         cmd.assert()
             .stdout(predicate::str::contains("Task no. 2123123 does not exist"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn zip_should_zip() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = initialize()?;
-        let input_file = make_input_file_cpp(&temp)?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
+        let input_file = make_input_file_cpp(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
-        cmd.current_dir(&temp);
         cmd.args(&[
             "submit",
             "-t",
@@ -209,15 +159,15 @@ mod submit {
         cmd.assert()
             .stdout(predicate::str::contains("Zipping source.cpp"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn default_option_should_save_task() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
         cmd.args(&[
             "submit",
             "-f",
@@ -228,7 +178,6 @@ mod submit {
             "Java",
             "--default",
         ]);
-        cmd.current_dir(&temp);
 
         cmd.assert()
             .stdout(predicate::str::contains("Task config has been saved."))
@@ -236,23 +185,23 @@ mod submit {
             .stdout(predicate::str::contains("Java"))
             .stdout(predicate::str::contains("[B] Metoda Newtona"));
 
-        let config_path = &*temp.path().join(".baca/task");
-        let does_exit_pred = predicate::path::exists().eval(config_path);
+        let config_path = dir.baca_task_config_file_path();
+        let does_exit_pred = predicate::path::exists().eval(&config_path);
         assert!(does_exit_pred);
-        let saved_config = fs::read_to_string(config_path)?;
+        let saved_config = fs::read_to_string(&config_path)?;
         assert!(saved_config.contains("dummy.txt"));
         assert!(saved_config.contains("Java"));
         assert!(saved_config.contains("2"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn saved_task_should_be_used() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
         cmd.args(&[
             "submit",
             "-f",
@@ -263,26 +212,24 @@ mod submit {
             "Java",
             "--default",
         ]);
-        cmd.current_dir(&temp);
         cmd.assert();
 
-        let mut cmd = Command::cargo_bin("baca")?;
+        let mut cmd = set_up_command(&dir)?;
         cmd.arg("submit");
-        cmd.current_dir(&temp);
         cmd.assert()
             .stdout(predicate::str::contains("Submitting dummy.txt"))
             .stdout(predicate::str::contains("Java"))
             .stdout(predicate::str::contains("[B] Metoda Newtona"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn cmd_options_should_override_saved_task() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
         cmd.args(&[
             "submit",
             "-f",
@@ -293,26 +240,24 @@ mod submit {
             "Java",
             "--default",
         ]);
-        cmd.current_dir(&temp);
         cmd.assert();
 
-        let mut cmd = Command::cargo_bin("baca")?;
+        let mut cmd = set_up_command(&dir)?;
         cmd.args(&["submit", "-f", "hello.cpp", "-l", "C++"]);
-        cmd.current_dir(&temp);
         cmd.assert()
             .stdout(predicate::str::contains("Submitting hello.cpp"))
             .stdout(predicate::str::contains("C++"))
             .stdout(predicate::str::contains("[B] Metoda Newtona"));
 
-        temp.close()?;
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn clear_should_remove_saved_task() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
         cmd.args(&[
             "submit",
             "-f",
@@ -323,33 +268,30 @@ mod submit {
             "Java",
             "--default",
         ]);
-        cmd.current_dir(&temp);
         cmd.assert();
 
-        assert_task_config_file_exists(&temp);
+        assert_task_config_file_exists(&dir);
 
-        let mut cmd = Command::cargo_bin("baca")?;
+        let mut cmd = set_up_command(&dir)?;
         cmd.arg("submit").arg("clear");
-        cmd.current_dir(&temp);
         cmd.assert();
 
-        assert_task_config_file_does_not_exist(&temp);
-        temp.close()?;
+        assert_task_config_file_does_not_exist(&dir);
+        dir.close()?;
         Ok(())
     }
 
     #[test]
     fn clear_on_already_clear_should_do_nothing() -> Result<(), Box<dyn std::error::Error>> {
-        let temp = initialize()?;
+        let dir = initialize_correct_workspace()?;
+        let mut cmd = set_up_command(&dir)?;
 
-        let mut cmd = Command::cargo_bin("baca")?;
         cmd.arg("submit").arg("clear");
-        cmd.current_dir(&temp);
         cmd.assert();
 
-        assert_task_config_file_does_not_exist(&temp);
-        assert_config_file_exists(&temp);
-        temp.close()?;
+        assert_task_config_file_does_not_exist(&dir);
+        assert_config_file_exists(&dir);
+        dir.close()?;
         Ok(())
     }
 }
