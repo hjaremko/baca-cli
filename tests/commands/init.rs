861d74faa2d3f654882c1280400cb602314e5b7b
Hubert Jaremko <hjaremko@outlook.com>
2021-10-24 20:55:58 +0200

    Save version info
    Close #27

--- tests/commands/init.rs
+++ tests/commands/init.rs
@@ -2,6 +2,7 @@ use crate::util::get_baca_credentials;
 use assert_cmd::Command;
 use assert_fs::TempDir;
 use predicates::prelude::*;
+use std::fs::read_to_string;
 
 fn baca_dir_exists(temp: &TempDir) -> bool {
     predicate::path::exists().eval(&*temp.path().join(".baca"))
@@ -63,6 +64,27 @@ fn success() -> Result<(), Box<dyn std::error::Error>> {
 
     assert!(baca_dir_exists(&temp));
     assert!(config_exists(&temp));
+    temp.close()?;
+    Ok(())
+}
+
+#[test]
+fn should_save_version() -> Result<(), Box<dyn std::error::Error>> {
+    let (login, pass, host) = get_baca_credentials();
+    let temp = assert_fs::TempDir::new()?;
+
+    let mut cmd = Command::cargo_bin("baca")?;
+
+    cmd.current_dir(&temp);
+    cmd.arg("-u");
+    cmd.arg("init")
+        .args(&["--host", &host, "-p", &pass, "-l", &login]);
+    cmd.assert().code(0);
+
+    let version_path = temp.path().join(".baca/version");
+    assert!(predicate::path::exists().eval(&version_path));
+    let saved_version = read_to_string(version_path).unwrap();
+    assert!(predicate::str::contains(env!("CARGO_PKG_VERSION")).eval(&saved_version));
     temp.close()?;
     Ok(())
 }
